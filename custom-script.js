322c115bb15fb550e42c5540da5455ab47d39980
Andrey <you@example.com>
2022-09-05 10:34:40 +0300

    finalize

--- custom-script.js
+++ custom-script.js
@@ -20,7 +20,18 @@ popUp.addEventListener('click', event => {
     }
 });
 
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
+
+const mainPriceButtonBlock = document.querySelector('.main-price-button-wrapper');
 
 const villaCards = document.querySelectorAll('.custom-villa-card');
 const mainPrice = document.querySelector('.main-price');
@@ -37,11 +48,34 @@ villaCards.forEach((card, index) => {
             currentCard.classList.remove('selected-card');
         }
         card.classList.add('selected-card');
-            villaNameContainer.innerHTML = event.target.querySelector('.st-link').innerHTML + '<br><span>Breakfast only</span>';
-            mainPrice.innerHTML = `<strong>£${priceList[index]}</strong>`;
-
+        villaNameContainer.innerHTML = event.target.querySelector('.st-link').innerHTML + '<br><span>Breakfast only</span>';
+        mainPrice.innerHTML = `<strong>£${priceList[index]}</strong>`;
+        if (card.classList.contains('sold-out')) {
+            mainPriceButtonBlock.innerHTML = `<h5 style="text-align: center; color: rgb(255, 101, 74); font-size: 18px;"
+                class="secure-now">
+                SOLD OUT! <br>Please, check other dates</h5>`;
+        } else {
+            mainPriceButtonBlock.innerHTML = `<a href="#" class="btn-show-price btn btn-primary upper mt5 book-btn-custom"
+            style="background-color: #ff7701; visibility: visible !important;">Book
+            Now</a>`;
+        }
     });
 });
+
+const villasContainer = document.querySelector('.container-villas');
+
+window.addEventListener('scroll', event => {
+    if ((villaNameContainer.offsetTop + 1000) > pageYOffset) {
+        mainPriceButtonBlock.innerHTML = `<a href="#" class="btn-show-price btn btn-primary upper mt5 book-btn-custom"
+        style="background-color: #ff7701; visibility: visible !important;">View villas</a>`;
+    }
+    else {
+        mainPriceButtonBlock.innerHTML = `<a href="#" class="btn-show-price btn btn-primary upper mt5 book-btn-custom"
+            style="background-color: #ff7701; visibility: visible !important;">Book
+            Now</a>`;
+    }
+});
+
 
 // window.addEventListener('scroll', event => {
 //     for (let i = 0; i < bookButtons.length; i++) {
@@ -59,18 +93,6 @@ villaCards.forEach((card, index) => {
 
 // });
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-
-
 
 const villaCardList = document.querySelectorAll('.custom-villa-card');
 const villaImages = document.querySelectorAll('.villa-image');
@@ -85,7 +107,6 @@ const searchWindow = document.querySelector('.custom-search-wrap');
 let sticky = searchBlock.offsetTop;
 
 
-
 window.addEventListener('scroll', event => {
     if (window.pageYOffset > sticky) {
         searchBlock.classList.add('sticky-search');
@@ -95,3 +116,4 @@ window.addEventListener('scroll', event => {
         searchWindow.classList.remove('noborder');
     }
 });
+
